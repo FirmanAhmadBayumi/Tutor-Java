1caca1f10b812898db680541af879a287f96a196
FIRMAN AHMAD BAYUMI <92419799+FirmanAhmadBayumi@users.noreply.github.com>
2022-09-05 20:27:35 +0700

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Soal_2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Biodata {
+    public static void main(String[] args){
+        String nama = JOptionPane.showInputDialog("Masukkan nama anda");
+        JOptionPane.showMessageDialog(null, "Halo " + nama);
+        
+        String alamat = JOptionPane.showInputDialog(null, "Masukkan alamat anda ");
+        JOptionPane.showMessageDialog(null, "Anda tinggal di " + alamat);
+        
+        String tahunAngkatan = JOptionPane.showInputDialog("Masukkan Tahun angkatan anda");
+        JOptionPane.showMessageDialog(null, "Tahun angkatan " + tahunAngkatan);
+    }
+}
