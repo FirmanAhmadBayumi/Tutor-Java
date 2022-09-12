ac0fc52047db6029f0ed84df886e42298f36ce8c
FIRMAN AHMAD BAYUMI <92419799+FirmanAhmadBayumi@users.noreply.github.com>
2022-09-12 20:52:37 +0700

    Update Tugasprak3.java

--- Tugasprak3.java
+++ Tugasprak3.java
@@ -1,22 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tugasprak3;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Firman Ahmad Bayumi
- * @npm 2117051062
- * @kelas B
- */
 public class Tugasprak3 {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int nilai;
